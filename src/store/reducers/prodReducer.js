44d75a5d35441de0a6edb37e1f055ffecb8c5947
Charles Azevedo <vx.charles@gmail.com>
2021-05-24 16:56:00 -0300

    Removed ADD_CART and EXCLUDE_CART from the reducer

--- src/store/reducers/prodReducer.js
+++ src/store/reducers/prodReducer.js
@@ -1,17 +1,12 @@
 const initialState = {
   loading: true,
-  prodList: [],
-  addCart: []
+  prodList: []
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_LIST_PROD":
       return { ...state, prodList: action.payload, loading: false }
-    case "ADD_CART":
-      return { ...state, addCart: action.payload }
-    case "EXCLUDE_CART":
-      return { ...state, addCart: action.payload }
     default:
       return state
   }
