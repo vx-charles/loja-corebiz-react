05ec99156bb40c3be61cbf1ccbb9e1c427e8d5f1
Charles Azevedo <vx.charles@gmail.com>
2021-05-24 16:58:18 -0300

    Removed the addCart, prodCartExclude and importing lodash

--- src/store/actions/prodActions.js
+++ src/store/actions/prodActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import _ from 'lodash'
 
 const prodAPI = 'https://corebiz-test.herokuapp.com/api/v1/products'
 
@@ -8,39 +7,4 @@ export const prodListApi = () => {
     const respProd = await axios.get(prodAPI)
     return dispatch({ type: 'GET_LIST_PROD', payload: respProd.data })
   }
-}
-
-export const addCart = addProd => {
-  
-  let prodFilter = addProd.filter(function (a) { // Evita de add produtos iguais.
-    return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
-  }, Object.create(null))
-  
-  addProd[addProd.length-1].add = true
-
-  localStorage.setItem('add_cart_corebiz', JSON.stringify(prodFilter))
-
-  return dispatch => {
-    return dispatch({ type: 'ADD_CART', payload: prodFilter })
-  }
-}
-
-export const prodCartExclude = (product, id) => {
-  console.log(product)
-  const newList = _.remove(product, function(obj){
-    if(obj.productId === id){
-      obj.add = false // add false dizendo que o produto foi excluído do carrinho
-    }
-    return !_.isEqual(id, obj.productId); //utiliza o isEqual para comparar
-  })
-
-  localStorage.setItem('add_cart_corebiz', JSON.stringify(newList))
-  
-  if(localStorage.getItem('add_cart_corebiz') === "[]") {
-    localStorage.removeItem('add_cart_corebiz')
-  }
-  
-  return dispatch => {
-    return dispatch({ type: 'EXCLUDE_CART', payload: newList })
-  } 
 }
