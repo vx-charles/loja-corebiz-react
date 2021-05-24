b40cbb47e1778b5404472d082088e2af2fbecc2d
Charles Azevedo <vx.charles@gmail.com>
2021-05-24 16:54:53 -0300

    Added useContext and useState for the components, to update the cart.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Produtos from './components/products'
 import Newsletter from './components/newsletter'
 import Footer from './components/footer'
 
+import { useState } from 'react'
+import AddProdContext from './contexts/contextAddProd'
+
 import './styles/global.scss'
 
 function App() {
+
+  const countProd = localStorage.getItem('add_cart_corebiz') !== null ? JSON.parse(localStorage.getItem('add_cart_corebiz')).length : 0
+  const [countCart, setCountCart] = useState(countProd)
+  const objCountCart = {countCart, setCountCart}
+
   return (
-    <>
+    <AddProdContext.Provider value={objCountCart}>
       <Header />
       <main>
         <Banner />
@@ -16,7 +24,7 @@ function App() {
         <Newsletter />
         <Footer />
       </main>
-    </>
+    </AddProdContext.Provider>
   )
 }
 
