c433d6bf9f710ea3310e71770f5c9c4a6b3878a0
Charles Azevedo <vx.charles@gmail.com>
2021-05-24 11:26:04 -0300

    Fixed the gulpfile.js file and added minified images

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 const gulp = require('gulp')
 const imagemin = require('gulp-imagemin')
 
-gulp.task('reduzir-imagens', function () {
+const tasksMinifyImg = [
+  'minify-images-footer',
+  'minify-images-banner',
+  'minify-images-header',
+  'minify-images-products'
+]
+
+gulp.task('minify-images-footer', async function () {
+  gulp.src('src/assets/images/footer/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/assets/images/footer/'))
+})
+
+gulp.task('minify-images-banner', async function () {
   gulp.src('src/assets/images/banners/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('src/assets/images/banners/**/*'))
+    .pipe(gulp.dest('src/assets/images/banners/'))
 })
+
+gulp.task('minify-images-header', async function () {
+  gulp.src('src/assets/images/header/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/assets/images/header/'))
+})
+
+gulp.task('minify-images-products', async function () {
+  gulp.src('src/assets/images/products/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('src/assets/images/products/'))
+})
+
+gulp.task('default', gulp.series(...tasksMinifyImg))
